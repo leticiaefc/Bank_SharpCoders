af69d9343b447d4fd1016f95f1f7dae921336059
Leticia Carregosa <leticiaevelynclmn@gmail.com>
2022-12-15 19:14:00 -0300

    segunda modificação

--- BankForAll/Program.cs
+++ BankForAll/Program.cs
@@ -1,38 +1,36 @@
-﻿namespace BankForAll;
+﻿namespace BankForAll {
 
-    public class Program
+public class Program
 {
     static void Menu()
     {
-        Console.WriteLine("1 - Inserir novo usuário");
-        Console.WriteLine("2 - Deletar usuário");
-        Console.WriteLine("3 - Detalhes de um usuário");
-        Console.WriteLine("4 - Total armazenado no banco");
+        Console.WriteLine("1 - Criar contar");
+        Console.WriteLine("2 - Deletar conta");
+        Console.WriteLine("3 - Listar todas as contas registradas");
+        Console.WriteLine("4 - Detalhes de um usuário");
+        Console.WriteLine("5 - Consultar Saldo");
+        Console.WriteLine("6 - Entrar na conta");
         Console.WriteLine("0 - Para sair do programa");
         Console.Write("Digite a opção desejada: ");
         // criar um comando par retornar uma ação
     }
+
     static void Main(string[] args)
     {
 
-        Console.WriteLine("Olá, bem vindo ao Bank For All.");
-        Console.Write("Digite a quantidade de usuários: ");
-        int qtdDeUsuarios = int.Parse(Console.ReadLine());
-        Console.WriteLine();
-                
-        int escolha, id;
-
-        do
-        {
+        Console.WriteLine("Olá, bem vindo ao Bank For All.\n");
+                    
+        List<string> cpfs = new List<string>();
+        List<string> titulares = new List<string>();
+        List<double> saldos = new List<double>();
+        List<string> senhas = new List<string>();
+        int escolha;
+          
+            do
+            {
             Menu();
             escolha = int.Parse(Console.ReadLine());
-            //if (escolha == 0)
-            //{
-            //    Console.WriteLine();
-            //    Console.WriteLine("Até mais!");
-            //} else {
-            //    Console.WriteLine("Selecione uma das opções acima");
-            //}
+
             switch (escolha)
             {
                 case 0:
@@ -40,29 +38,51 @@
                     break;
                 case 1:
                     Console.WriteLine();
-                    Console.WriteLine("Certo, vamos adicionar algumas informações.\nPreencha os dados solicitados\n");
-                    Console.Write("número identificador: "); // cpf,
-                    id = int.Parse(Console.ReadLine());
-                    Console.Write("idade:");
-                    int idade = int.Parse(Console.ReadLine());
-
-
+                    Console.WriteLine(" Precisamos de  algumas informações.\nPreencha os dados solicitados:\n");
+                    RegistroNovoUsuario(cpfs, titulares, senhas, saldos);
+                    Console.WriteLine();
                     break;
-                case 2: Console.Write("Digite o nome do usuário que deseja deletar: ");
+                case 2:
+                    Console.Write("Digite o cpf do usuário que deseja deletar: ");
                     string nome = Console.ReadLine();
+                        break;
+                case 3:
+                        Console.WriteLine();
+                        ListarTodasAsContas(cpfs, titulares, saldos);
                     break;
             }
 
-            // ação que a pessoa pediu, provavelmente com if
+
             Console.WriteLine();
             Console.WriteLine("-/-/-/-/-/-");
             Console.WriteLine();
-            
+
 
         } while (escolha != 0);
 
-
     }
+
+    static void RegistroNovoUsuario(List<string> cpfs, List<string> titulares, List<string> senhas, List<double> saldos)
+    {
+        Console.Write("Digite o cpf: ");
+        cpfs.Add(Console.ReadLine());
+        Console.Write("Digite o nome: ");
+        titulares.Add(Console.ReadLine());
+        Console.Write("Digite uma senha: ");
+        senhas.Add(Console.ReadLine());
+        Console.Write("Seu saldo: ");
+        saldos.Add(0);
+    }
+
+    static void ListarTodasAsContas(List<string> cpfs, List<string> titulares, List<double> saldos)
+    {
+        for (int i = 0; i < cpfs.Count; i++)
+        {
+            Console.WriteLine($"CPF = {cpfs[i]} | Titular = {titulares[i]} | Saldo = R$ {saldos[i]:F2}");
+        }
+    }
+
 }
 
+}
 
